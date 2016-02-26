1860eb2bdb4010719d5d185c67480536a79474c1
Khalid Omar Ali <khalid.ali@consultant.com>
2016-02-26 23:58:56 +0000

    add project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject drawing "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Drawing Application"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
